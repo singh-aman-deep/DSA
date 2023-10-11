801a9146802589ed2c46024fcc5fa33fbdd146c9
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-11 15:03:41 +0530

    Leet Code || 11 Oct 2023 || 2180. Count Integers With Even Digit Sum

---
+++ src/com/dsa/leetcode/CountIntegersWithEvenDigitSum.java
@@ -0,0 +1,44 @@
+package com.dsa.leetcode;
+
+/**
+ * Given a positive integer num, return the number of positive integers less than or equal to num whose digit sums are even.
+ *
+ * The digit sum of a positive integer is the sum of all its digits.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: num = 4
+ * Output: 2
+ * Explanation:
+ * The only integers less than or equal to 4 whose digit sums are even are 2 and 4.
+ */
+public class CountIntegersWithEvenDigitSum {
+    public static void main(String[] args) {
+        int input =500;
+        System.out.println("Result->"+countEven(input));
+    }
+
+    public static int countEven(int num) {
+        int count=0;
+        for (int i=1;i<=num;i++){
+            if (sumOfDigits(i)%2==0){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int sumOfDigits(int number) {
+        int sum = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+
+        return sum;
+    }
+}
