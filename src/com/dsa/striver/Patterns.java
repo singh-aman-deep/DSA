0e5357f92f8c0a5ad3448d22c50c0e1ed72f368d
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-17 18:19:44 +0530

    Striver || 17 Oct 2023 || Patterns-8

---
+++ src/com/dsa/striver/Patterns.java
@@ -0,0 +1,35 @@
+package com.dsa.striver;
+
+public class Patterns {
+    public static void main(String[] args) {
+        int n = 3;
+        pattern8(n);
+    }
+
+    public static void pattern8(int n) {
+// This is the outer loop which will loop for the rows.
+        for (int i = 0; i < n; i++) {
+            // For printing the spaces before stars in each row
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            // For printing the stars in each row
+            for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+
+                System.out.print("*");
+            }
+
+            // For printing the spaces after the stars in each row
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+
+            // As soon as the stars for each iteration are printed, we move to the
+            // next row and give a line break otherwise all stars
+            // would get printed in 1 line.
+            System.out.println();
+        }
+    }
+}
