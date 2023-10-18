ac85c2e92060a8fbdd167a520511ee745915e3c9
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-18 12:14:18 +0530

    Striver || 18 Oct 2023 || Pattern 14

--- src/com/dsa/striver/Patterns.java
+++ src/com/dsa/striver/Patterns.java
@@ -9,7 +9,8 @@ public class Patterns {
 //        pattern10(n);
 //        pattern11(n);
 //        pattern12(n);
-        pattern13(n);
+//        pattern13(n);
+        pattern14(n);
     }
 
     /**
@@ -219,6 +220,28 @@ public class Patterns {
             }
 
             System.out.println();
+        }
+    }
+
+    /**
+     * A
+     * A B
+     * A B C
+     */
+    private static void pattern14(int n) {
+        for(int i=0;i<n;i++){
+
+            // Inner loop will loop for i times and
+            // print alphabets from A to A + i.
+            for(char ch = 'A'; ch<='A'+i;ch++){
+                System.out.print(ch + " ");
+
+            }
+            // As soon as the letters for each iteration are printed, we move to the
+            // next row and give a line break otherwise all letters
+            // would get printed in 1 line.
+            System.out.println();
+
         }
     }
 }
