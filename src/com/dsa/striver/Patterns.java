2c433e49628c400f173cb23bc50812d03b40b35b
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-18 11:39:17 +0530

    Striver || 18 Oct 2023 || Pattern 9,10,11

--- src/com/dsa/striver/Patterns.java
+++ src/com/dsa/striver/Patterns.java
@@ -3,9 +3,50 @@ package com.dsa.striver;
 public class Patterns {
     public static void main(String[] args) {
         int n = 3;
-        pattern8(n);
+//        pattern7(n);
+//        pattern8(n);
+//        pattern9(n);
+//        pattern10(n);
+        pattern11(n);
     }
 
+    /**
+     *   *
+     *  ***
+     * *****
+     */
+    static void pattern7(int n) {
+        // This is the outer loop which will loop for the rows.
+        for (int i = 0; i < n; i++) {
+            // For printing the spaces before stars in each row
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            // For printing the stars in each row
+            for (int j = 0; j < 2 * i + 1; j++) {
+
+                System.out.print("*");
+            }
+
+            // For printing the spaces after the stars in each row
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+
+            // As soon as the stars for each iteration are printed, we move to the
+            // next row and give a line break otherwise all stars
+            // would get printed in 1 line.
+            System.out.println();
+        }
+    }
+
+    /**
+     * *****
+     *  ***
+     *   *
+     */
     public static void pattern8(int n) {
 // This is the outer loop which will loop for the rows.
         for (int i = 0; i < n; i++) {
@@ -29,6 +70,109 @@ public class Patterns {
             // As soon as the stars for each iteration are printed, we move to the
             // next row and give a line break otherwise all stars
             // would get printed in 1 line.
+            System.out.println();
+        }
+    }
+
+    /**
+     *   *
+     *  ***
+     * *****
+     * *****
+     *  ***
+     *   *
+     */
+    private static void pattern9(int n) {
+        for (int i = 0; i < n; i++) {
+            // For printing the spaces before stars in each row
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            // For printing the stars in each row
+            for (int j = 0; j < 2 * i + 1; j++) {
+
+                System.out.print("*");
+            }
+
+            // For printing the spaces after the stars in each row
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+
+            // As soon as the stars for each iteration are printed, we move to the
+            // next row and give a line break otherwise all stars
+            // would get printed in 1 line.
+            System.out.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+            // For printing the spaces before stars in each row
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            // For printing the stars in each row
+            for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+
+                System.out.print("*");
+            }
+
+            // For printing the spaces after the stars in each row
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+
+            // As soon as the stars for each iteration are printed, we move to the
+            // next row and give a line break otherwise all stars
+            // would get printed in 1 line.
+            System.out.println();
+        }
+    }
+
+    /**
+     *   *
+     *   **
+     *   ***
+     *   **
+     *   *
+     */
+    private static void pattern10(int n) {
+        for (int i=0;i<n;i++){
+
+            for (int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i=0;i<n;i++){
+            for (int j=0;j<=n-i-2;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+    /**
+     * 1
+     * 01
+     * 101
+     */
+    private static void pattern11(int n) {
+        for (int i=0;i<n;i++){
+
+            for (int j=0;j<=i;j++){
+                if (i%2==0){
+                    System.out.print(j%2==0?1:0);
+                    System.out.print(" ");
+                }else {
+                    System.out.print(j%2==0?0:1);
+                    System.out.print(" ");
+                }
+            }
             System.out.println();
         }
     }
