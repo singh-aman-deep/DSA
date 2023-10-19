77168f2537a1d6e768ce872244f07b2182accd71
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-19 11:56:18 +0530

    Striver || 19 Oct 2023 || Patterns 18

--- src/com/dsa/striver/Patterns.java
+++ src/com/dsa/striver/Patterns.java
@@ -13,8 +13,10 @@ public class Patterns {
 //        pattern14(n);
 //        pattern15(n);
 //        pattern16(n);
-        pattern17(n);
+//        pattern17(n);
+        pattern18(n);
     }
+
 
 
     /**
@@ -324,5 +326,28 @@ public class Patterns {
 
         }
 
+    }
+
+    /**
+     * C
+     * B C
+     * A B C
+     */
+    private static void pattern18(int n) {
+        // Outer loop for the no. of rows.
+        for(int i=0;i<n;i++){
+
+            // Inner loop for printing the alphabets from
+            // A + n -1 -i (i is row no.) to A + n -1 ( E in this case).
+            for(char ch =(char)(int)('A'+n-1-i);ch<=(char)(int)('A'+n-1);ch++){
+
+                System.out.print(ch + " ");
+            }
+
+            // As soon as the letters for each iteration are printed, we move to the
+            // next row and give a line break otherwise all letters
+            // would get printed in 1 line.
+            System.out.println();
+        }
     }
 }
