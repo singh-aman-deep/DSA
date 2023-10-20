8123294ac968d6e1f7f9b81bbc8dfd28d09e3d8a
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-20 11:07:09 +0530

    Striver || 20 Oct 2023 || Patterns 19

--- src/com/dsa/striver/Patterns.java
+++ src/com/dsa/striver/Patterns.java
@@ -14,9 +14,9 @@ public class Patterns {
 //        pattern15(n);
 //        pattern16(n);
 //        pattern17(n);
-        pattern18(n);
+//        pattern18(n);
+        pattern19(n);
     }
-
 
 
     /**
@@ -343,6 +343,76 @@ public class Patterns {
 
                 System.out.print(ch + " ");
             }
+
+            // As soon as the letters for each iteration are printed, we move to the
+            // next row and give a line break otherwise all letters
+            // would get printed in 1 line.
+            System.out.println();
+        }
+    }
+
+    /**
+     * ******
+     * **  **
+     * *    *
+     * *    *
+     * **  **
+     * ******
+     */
+    private static void pattern19(int n) {
+        // for the upper half of the pattern.
+
+        // initial spaces.
+        int iniS = 0;
+        for(int i=0;i< n;i++){
+
+            //for printing the stars in the row.
+            for(int j=1;j<=n-i;j++){
+                System.out.print("*");
+            }
+
+            //for printing the spaces in the row.
+            for(int j=0;j<iniS;j++){
+                System.out.print(" ");
+            }
+
+            //for printing the stars in the row.
+            for(int j=1;j<=n-i;j++){
+                System.out.print("*");
+            }
+
+            // The spaces increase by 2 every time we hit a new row.
+            iniS+=2;
+
+            // As soon as the letters for each iteration are printed, we move to the
+            // next row and give a line break otherwise all letters
+            // would get printed in 1 line.
+            System.out.println();
+        }
+
+        // for lower half of the pattern
+
+        // initial spaces.
+        iniS = 2*n -2;
+        for(int i=1;i<=n;i++){
+
+            //for printing the stars in the row.
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+
+            //for printing the spaces in the row.
+            for(int j=0;j<iniS;j++){
+                System.out.print(" ");
+            }
+
+            //for printing the stars in the row.
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+
+            // The spaces decrease by 2 every time we hit a new row.
+            iniS-=2;
 
             // As soon as the letters for each iteration are printed, we move to the
             // next row and give a line break otherwise all letters
