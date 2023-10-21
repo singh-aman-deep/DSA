445aa8d5aefde02880a4558c48194b89c8124074
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-21 12:06:08 +0530

    GFG || 21 Oct 2023 || Sum of Array Elements

---
+++ src/com/dsa/gfg/SumofArrayElements.java
@@ -0,0 +1,26 @@
+package com.dsa.gfg;
+
+/**
+ * 21 Oct 2023
+ * <p>
+ * Sum of Array Elements
+ * <p>
+ * Given an integer array arr of size n, you need to sum the elements of arr.
+ */
+public class SumofArrayElements {
+
+    public static void main(String[] args) {
+        int n = 3;
+        int arr[] = new int[]{3, 2, 1};
+        System.out.println(sumElement(arr,n));
+
+    }
+
+    public static int sumElement(int arr[], int n) {
+        int output=0;
+        for (int i = 0; i < n; i++) {
+            output=output+arr[i];
+        }
+        return output;
+    }
+}
