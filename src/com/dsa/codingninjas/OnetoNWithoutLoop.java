24065ee100df55b7a8deb71c0fec12760da4b32c
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-22 13:11:33 +0530

    Coding Ninjas || 22 Oct 2023 || 1 to N Without Loop

---
+++ src/com/dsa/codingninjas/OnetoNWithoutLoop.java
@@ -0,0 +1,36 @@
+package com.dsa.codingninjas;
+
+import java.util.Arrays;
+
+/**
+ * 22 Oct 2023
+ *
+ * 1 to N Without Loop
+ *
+ * You are given an integer n
+ * Your task is to return an array containing integers from 1 to ‘n’ (in increasing order) without using loops.
+ *
+ * Input: ‘n’ = 5
+ *
+ * Output: 1 2 3 4 5
+ *
+ * Explanation: An array containing integers from ‘1’ to ‘n’ is [1, 2, 3, 4, 5].
+ */
+public class OnetoNWithoutLoop {
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(Arrays.toString(printNos(n)));
+    }
+
+    public static int[] printNos(int x) {
+        int [] a=new int[x];
+        return recur(x,a);
+    }
+
+    public static int[]recur(int x,int[]a){
+        if(x==0) return a;
+        int i=x;
+        a[i-1]=x;
+        return recur(x-1,a);
+    }
+}
