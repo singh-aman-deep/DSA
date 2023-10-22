47d966eb76f8ae604d50849ef985d35537c6efdc
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-22 12:22:41 +0530

    Striver || 22 Oct 2023 ||  Find GCD of two numbers

---
+++ src/com/dsa/striver/GCDOfTwoNumbers.java
@@ -0,0 +1,17 @@
+package com.dsa.striver;
+
+public class GCDOfTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 6;
+        System.out.println(calcGCD(num1, num2));
+
+    }
+
+    public static int calcGCD(int n, int m) {
+        if (m==0){
+            return n;
+        }
+        return calcGCD(m,n%m);
+    }
+}
