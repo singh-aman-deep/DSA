51f93ff0c1f57ba637f0fe6f9e1ae69ec13ee7ad
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-23 12:27:50 +0530

    Leet Code || 23 Oct 2023 || 242. Valid Anagram

---
+++ src/com/dsa/leetcode/ValidAnagram.java
@@ -0,0 +1,37 @@
+package com.dsa.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 23 Oct 2023
+ * <p>
+ * 242. Valid Anagram
+ * <p>
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ * <p>
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ */
+public class ValidAnagram {
+    public static void main(String[] args) {
+//        String s = "anagram";
+//        String t = "nagaram";
+        String s = "rat";
+        String t = "car";
+        System.out.println(isAnagram(s, t));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        char[] one = s.toCharArray();
+        char[] two = t.toCharArray();
+        Arrays.sort(one);
+        Arrays.sort(two);
+        return Arrays.equals(one,two);
+    }
+}
