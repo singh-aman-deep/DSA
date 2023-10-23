975e25753b9c9d85cac60034d357969c239a3d35
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-23 12:28:14 +0530

    Leet Code || 23 Oct 2023 || 242. Valid Anagram

--- src/com/dsa/leetcode/ValidAnagram.java
+++ src/com/dsa/leetcode/ValidAnagram.java
@@ -11,8 +11,6 @@ import java.util.Arrays;
  * <p>
  * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
  * <p>
- * <p>
- * <p>
  * Example 1:
  * <p>
  * Input: s = "anagram", t = "nagaram"
