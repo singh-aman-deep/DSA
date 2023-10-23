a5e3448daa677ddf739a5017a3ee81e04c2480ea
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-23 16:39:46 +0530

    Striver || 23 Oct 2023 || Factorial of a Number

---
+++ src/com/dsa/striver/FactorialOfANumber.java
@@ -0,0 +1,27 @@
+package com.dsa.striver;
+
+/**
+ * 23 Oct 2023
+ * <p>
+ * Factorial of a Number
+ * <p>
+ * Problem Statement: Given a number X,  print its factorial.
+ * <p>
+ * Example 1:
+ * Input: X = 5
+ * Output: 120
+ * Explanation: 5! = 5*4*3*2*1
+ */
+public class FactorialOfANumber {
+    public static void main(String[] args) {
+        int x = 3;
+        System.out.println(fact(x));
+    }
+
+    public static int fact(int x) {
+        if (x<1){
+            return 1;
+        }
+        return x * fact(x - 1);
+    }
+}
