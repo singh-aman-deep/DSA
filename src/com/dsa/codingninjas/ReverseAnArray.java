7de8d769ba95e133af81a25bf2fe883e48e11a4f
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-23 16:40:36 +0530

    Coding Ninjas || 23 Oct 2023 || Reverse an Array

--- src/com/dsa/codingninjas/ReverseAnArray.java
+++ src/com/dsa/codingninjas/ReverseAnArray.java
@@ -7,7 +7,11 @@ import java.util.Arrays;
  *
  * Reverse an Array
  *
+ * Problem Statement: You are given an array. The task is to reverse the array and print it.
  *
+ * Example 1:
+ * Input: N = 5, arr[] = {5,4,3,2,1}
+ * Output: {1,2,3,4,5}
  */
 public class ReverseAnArray {
     public static void main(String[] args) {
