2f44e0f72c710bd4bf333f6cc772b7f07a762bdb
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-23 17:00:00 +0530

    Striver || 23 Oct 2023 ||  Reverse a given Array Recursive method

---
+++ src/com/dsa/striver/ReverseAGivenArray.java
@@ -0,0 +1,32 @@
+package com.dsa.striver;
+
+import java.util.Arrays;
+
+/**
+ * 23 Oct 2023
+ *
+ * Reverse a given Array Recursive method
+ *
+ * Problem Statement: You are given an array. The task is to reverse the array and print it.
+ *
+ * Example 1:
+ * Input: N = 5, arr[] = {5,4,3,2,1}
+ * Output: {1,2,3,4,5}
+ */
+public class ReverseAGivenArray {
+    public static void main(String[] args) {
+        int n = 5;
+        int arr[] = {5,4,3,2,1};
+        reverseArray(arr,0,n-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void reverseArray(int[] arr, int start, int end) {
+        if (start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            reverseArray(arr, start+1, end-1);
+        }
+    }
+}
