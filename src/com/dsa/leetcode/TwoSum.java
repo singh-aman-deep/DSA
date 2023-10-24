e7444e3023d1e2ac2db270d87558ed5436a08f9e
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-24 12:11:08 +0530

    Leet Code || 24 Oct 2023 ||  1269. 1. Two Sum

---
+++ src/com/dsa/leetcode/TwoSum.java
@@ -0,0 +1,40 @@
+package com.dsa.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 24 Oct 2023
+ * 1. Two Sum
+ * <p>
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * <p>
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * You can return the answer in any order.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {3,2,4};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{}; // No solution found
+    }
+}
