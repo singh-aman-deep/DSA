8ea200de1e3d9b4f7b23bd0d3def7381bafd44e4
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-25 11:48:09 +0530

    GFG || 25 Oct 2023 || Print Elements of Array

---
+++ src/com/dsa/gfg/PrintElementsofArray.java
@@ -0,0 +1,29 @@
+package com.dsa.gfg;
+
+/**
+ * 25 Oct 2023
+ * Print Elements of Array
+ *
+ * Given an array Arr of size N, print all its elements.
+ *
+ * Example 1:
+ *
+ * Input:
+ * N = 5
+ * Arr[] = {1, 2, 3, 4, 5}
+ * Output: 1 2 3 4 5
+ */
+public class PrintElementsofArray {
+    public static void main(String[] args) {
+        int n = 5;
+        int arr[] = {1, 2, 3, 4, 5} ;
+        printArray(arr,n);
+
+    }
+
+    static void printArray(int arr[], int n) {
+        for (int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
