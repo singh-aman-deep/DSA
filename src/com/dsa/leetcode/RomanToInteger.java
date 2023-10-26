0a44f27efd289b423710ca4bb7d6f1f3e181a58f
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-26 12:45:08 +0530

    Leet Code || 26 Oct 2023 ||  13. Roman to Integer

--- src/com/dsa/leetcode/RomanToInteger.java
+++ src/com/dsa/leetcode/RomanToInteger.java
@@ -78,4 +78,46 @@ public class RomanToInteger {
 
         return ans;
     }
+
+    class Solution {
+        public int romanToInt(String s) {
+            char[] charArray = s.toCharArray();
+            int result = 0;
+            int prevValue = 0;
+            for (int i = charArray.length - 1; i >= 0; i--) {
+                int currentValue = getValue(charArray[i]);
+
+                if (currentValue >= prevValue) {
+                    result += currentValue;
+                } else {
+                    result -= currentValue;
+                }
+
+                prevValue = currentValue;
+            }
+
+            return result;
+        }
+
+        public int getValue(char c) {
+            switch (c) {
+                case 'I':
+                    return 1;
+                case 'V':
+                    return 5;
+                case 'X':
+                    return 10;
+                case 'L':
+                    return 50;
+                case 'C':
+                    return 100;
+                case 'D':
+                    return 500;
+                case 'M':
+                    return 1000;
+                default:
+                    return 0;
+            }
+        }
+    }
 }
