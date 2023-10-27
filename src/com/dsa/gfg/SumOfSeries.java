cc61f17abcabaaaf181b41bf282e7acce342691f
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-27 12:42:26 +0530

    GFG || 27 Oct 2023 || Sum of Series

---
+++ src/com/dsa/gfg/SumOfSeries.java
@@ -0,0 +1,27 @@
+package com.dsa.gfg;
+
+/**
+ * 27 Oct 2023
+ *
+ * Sum of Series
+ *
+ * Write a program to find the sum of the given series 1+2+3+ . . . . . .(N terms)
+ *
+ * Example 1:
+ *
+ * Input:
+ * N = 1
+ * Output: 1
+ * Explanation: For n = 1, sum will be 1.
+ *
+ */
+public class SumOfSeries {
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(seriesSum(n));
+    }
+
+    static long seriesSum(int n) {
+        return (long) ((double) n /2*(n+1));
+    }
+}
