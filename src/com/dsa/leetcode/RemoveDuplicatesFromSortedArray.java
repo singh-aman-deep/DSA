0f05c5607884e0abdafe9e80126c1a38b2076df7
Amandeep Singh <aman.harry2744@gmail.com>
2023-10-29 09:30:17 +0530

    Leet Code || 29 Oct 2023 ||1662. 26. Remove Duplicates from Sorted Array

---
+++ src/com/dsa/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,39 @@
+package com.dsa.leetcode;
+
+/**
+ * 29 Oct 2023
+ * <p>
+ * 26. Remove Duplicates from Sorted Array
+ * <p>
+ * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+ * <p>
+ * Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+ * <p>
+ * Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+ * Return k.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,1,2]
+ * Output: 2, nums = [1,2,_]
+ * Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+ * It does not matter what you leave beyond the returned k (hence they are underscores).
+ */
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+
+        int [] nums = {1,1,2};
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
