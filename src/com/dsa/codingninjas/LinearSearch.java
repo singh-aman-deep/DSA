3c90262b08bf163a506db7e3255eda41639508a5
Amandeep Singh <aman.harry2744@gmail.com>
2023-11-02 11:20:32 +0530

    Coding Ninja || 1st Nov || Linear Search

--- src/com/dsa/codingninjas/LinearSearch.java
+++ src/com/dsa/codingninjas/LinearSearch.java
@@ -1,5 +1,23 @@
 package com.dsa.codingninjas;
 
+/**
+ *1 Nov 2023
+ *
+ * Linear Search
+ *
+ * You are given an array 'arr'  containing  ‘n’  integers. You are also given an integer ‘num’ , and your task is to find whether ‘num’ is present in the array or not.
+ * If ‘num’ is present in the array, return the 0-based index of the first occurrence of ‘num’. Else, return -1.
+ *
+ *Example:
+ *
+ * Input: ‘n’ = 5, ‘num’ = 4
+ * 'arr' =  [6,7,8,4,1]
+ *
+ * Output: 3
+ *
+ * Explanation:
+ * 4 is present at the 3rd index.
+ */
 public class LinearSearch {
 
     public static void main(String[] args) {
