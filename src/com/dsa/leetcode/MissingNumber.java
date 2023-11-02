bf83ce70bd437c242173bfce8fde484cf7794147
Amandeep Singh <aman.harry2744@gmail.com>
2023-11-02 11:16:14 +0530

    Leet Code || 1st Nov || 268. Missing Number

--- src/com/dsa/leetcode/MissingNumber.java
+++ src/com/dsa/leetcode/MissingNumber.java
@@ -2,6 +2,25 @@ package com.dsa.leetcode;
 
 import java.util.Arrays;
 
+/**
+ * 1 Nov 2023
+ *
+ * 268. Missing Number
+ *
+ *
+ * Given an array nums containing n distinct numbers in the range [0, n], return the only number in the
+ * range that is missing from the array.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [3,0,1]
+ * Output: 2
+ * Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3].
+ * 2 is the missing number in the range since it does not appear in nums.
+ *
+ */
 public class MissingNumber {
 
     public static void main(String[] args) {
