0189f6031c38b135372ecee3def3e9b0b5823139
Amandeep Singh <aman.harry2744@gmail.com>
2023-11-02 11:17:11 +0530

    GFG || 1st Nov || FascinatingNumber

--- src/com/dsa/gfg/FascinatingNumber.java
+++ src/com/dsa/gfg/FascinatingNumber.java
@@ -1,5 +1,25 @@
 package com.dsa.gfg;
 
+/**
+ * 1 Nov 2023
+ *
+ * Fascinating Number
+ *
+ * Given a number N. Your task is to check whether it is fascinating or not.
+ * Fascinating Number: When a number(should contain 3 digits or more) is multiplied by 2 and 3,
+ * and when both these products are concatenated with the original number, then it results in all digits
+ * from 1 to 9 present exactly once.
+ *
+ * Example 1:
+ *
+ * Input:
+ * N = 192
+ * Output: Fascinating
+ * Explanation: After multiplication with 2
+ * and 3, and concatenating with original
+ * number, number will become 192384576
+ * which contains all digits from 1 to 9.
+ */
 public class FascinatingNumber {
 
     public static void main(String[] args) {
