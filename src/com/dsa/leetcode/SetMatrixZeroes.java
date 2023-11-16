7f04e1f0cf3a8125d5800da9fb6402c297f12076
Amandeep Singh <aman.harry2744@gmail.com>
2023-11-16 12:51:18 +0530

    Leet Code || 16 Nov 2023 ||  73. Set Matrix Zeroes

---
+++ src/com/dsa/leetcode/SetMatrixZeroes.java
@@ -0,0 +1,61 @@
+package com.dsa.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 16 Nov 2023
+ * <p>
+ * 73. Set Matrix Zeroes
+ * <p>
+ * Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+ * <p>
+ * You must do it in place.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+ * Output: [[1,0,1],[0,0,0],[1,0,1]]
+ * Example 2:
+ * <p>
+ * Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+ * Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ */
+public class SetMatrixZeroes {
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        setZeroes(matrix);
+        System.out.println(Arrays.toString(matrix));
+    }
+
+    public static void setZeroes(int[][] matrix) {
+        boolean fr = false, fc = false;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) fr = true;
+                    if (j == 0) fc = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (fr) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (fc) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
