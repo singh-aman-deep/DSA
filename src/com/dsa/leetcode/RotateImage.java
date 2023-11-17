b91e72ecdfcef3ee98d0da3ac5f8d58344b44c94
Amandeep Singh <aman.harry2744@gmail.com>
2023-11-17 11:30:53 +0530

    Leet Code || 17 Nov 2023 ||  48. Rotate Image

---
+++ src/com/dsa/leetcode/RotateImage.java
@@ -0,0 +1,54 @@
+package com.dsa.leetcode;
+
+/**
+ * 17 Nov 2023
+ * <p>
+ * 48. Rotate Image
+ * <p>
+ * You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+ * <p>
+ * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+ * <p>
+ * Example:
+ * <p>
+ * Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * Output: [[7,4,1],[8,5,2],[9,6,3]]
+ */
+public class RotateImage {
+
+    public static void main(String[] args) {
+        int arr[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate(arr);
+        System.out.println("Rotated Image");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Time Complexity: O(N*N) + O(N*N).One O(N*N) is for transposing the matrix and the other is for reversing the matrix.
+     * <p>
+     * Space Complexity: O(1).
+     */
+    public static void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+}
